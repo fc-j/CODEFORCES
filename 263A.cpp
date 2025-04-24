4ad4b80b9e839808984c5c03f369d9730e8a1fb5
fc-j <francisco.brokz@gmail.com>
2025-04-24 18:50:54 -0500

    agregando ejercicio

---
+++ 263A.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std; 
+int main (){
+    int cont =0; 
+    int arr[5][5]={}; 
+    for (int i=0; i <5; i++){
+    
+        for(int j=0; j < 5; j++){
+            cin>>arr[i][j]; 
+        }
+    }
+    for (int i=0; i <5; i++){
+    
+        for(int j=0; j < 5; j++){
+            if(arr[i][j]==1){
+                if(i>2){
+                    cont+=i-2; 
+                }
+                else {
+                    cont+=2-i; 
+                } 
+                if(j>2){
+                     cont+=j-2;      
+                }
+                else{
+                    cont +=2-j; 
+                }
+            }
+
+        }
+    }
+    cout<<cont; 
+}
